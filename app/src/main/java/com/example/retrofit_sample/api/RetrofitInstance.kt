e0831f3ab27a53e0083ece83546a25a0f69ad91d
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-08-24 12:51:47 +0530

    bug fix: KotlinJsonAdapterFactory used with moshi

--- app/src/main/java/com/example/retrofit_sample/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/retrofit_sample/api/RetrofitInstance.kt
@@ -1,15 +1,19 @@
 package com.example.retrofit_sample.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 object RetrofitInstance {
 
+    private val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 
